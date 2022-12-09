d730a5eddcde4867161f0d9c475f2fff77b9b06d
Elias <elianamos29@gmail.com>
2022-12-09 12:44:30 -0800

    Combine numbers 0 - 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,30 +7,25 @@
 
 int main(void)
 {
-	int num, num2, num3, num4;
+	int num, num2;
 
-	for (num = 48; num < 58; num++)
+	for (num = 0; num <= 98; num++)
 	{
-		for (num2 = num; num2 < 57; num2++)
+		for (num2 = num + 1; num2 < 99; num2++)
 		{
-			for (num3 = num2; num3 < 58; num3++)
-			{
-				for (num4 = 49; num4 < 58; num4++)
-				{
-					putchar(num);
-					putchar(num2);
-					putchar(' ');
-					putchar(num3);
-					putchar(num4);
-					if (num == 57 && num2 == 56 && num3 == 57 && num4 == 57)
-						break;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if ((i == 98) && (k == 99))
+				break;
+			putchar(',');
+			putchar(' ');
 		}
 	}
-
 	putchar('\n');
+
 	return (0);
 }
