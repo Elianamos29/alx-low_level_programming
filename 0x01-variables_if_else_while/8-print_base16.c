73c479fe2f22ff00aba47a9e00761069b11bea0f
Elias <elianamos29@gmail.com>
2022-12-09 08:35:11 -0800

    base hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print hexadecimal base numbers in lower case
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 48; c <= 58; c++)
+		putchar(c);
+	for (c = 97; c <= 102; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
