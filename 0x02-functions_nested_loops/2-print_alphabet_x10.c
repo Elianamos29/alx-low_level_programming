b322aee906c6c89371041722f17ac44c6b90515a
Elias <elianamos29@gmail.com>
2022-12-13 09:24:54 -0800

    10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase 10 times
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+		for (j = 97; j <= 122; j++)
+			_putchar(j);
+	_putchar('\n');
+}
