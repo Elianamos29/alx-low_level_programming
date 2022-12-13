b8bbeb1957881007b00541514bd54cde3d1b18b3
Elias <elianamos29@gmail.com>
2022-12-13 09:58:11 -0800

    absolute value of anumber

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value of a number
+ * @n: number input
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (0 - i);
+
+}
