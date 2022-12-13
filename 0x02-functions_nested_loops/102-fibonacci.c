201a1aebebc30c95b1dba1b51eb9427db1867a81
Elias <elianamos29@gmail.com>
2022-12-13 13:12:43 -0800

    first 50 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci numbers
+ * Return: void
+ */
+
+int main(void)
+{
+	long int firstn, secondn, thirdn, i;
+
+	firstn = 1;
+	secondn = 2;
+	printf("%ld, ", firstn);
+	printf("%ld, ", secondn);
+	for (i = 3; i < 50; i++)
+	{
+		thirdn = firstn + secondn;
+		printf("%ld ,", thirdn);
+		firstn = secondn;
+		secondn = thirdn;
+	}
+	printf("%ld\n", thirdn);
+	return (0);
+}
