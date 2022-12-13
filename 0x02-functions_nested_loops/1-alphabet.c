bdb0026b5558612a03df8240bfb4f49035274fd5
Elias <elianamos29@gmail.com>
2022-12-13 09:18:08 -0800

    Alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+}
