aab9f7605f983065762a8d19960667deef92c7a7
Elias <elianamos29@gmail.com>
2022-12-14 10:47:54 -0800

    Debugging task 0 negative or positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks whether a number is negative or positve
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	positive_or_negative(num);
+
+	return (0);
+}
