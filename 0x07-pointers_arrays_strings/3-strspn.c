439ed3cbe3db2e846f1357b4bc406aa6b695ca92
Elias <elianamos29@gmail.com>
2023-01-02 04:56:39 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * _strspn - gets the length of prefix substring only having a certain bytes
  * @s: a string
  * @accept: an input string
- * Returns: length of prefix substring
+ * Return: length of prefix substring
  */
 
 unsigned int _strspn(char *s, char *accept)
