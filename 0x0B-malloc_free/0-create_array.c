3a254b2cd1341ad37dc52929802dcdde597ea945
Elias <elianamos29@gmail.com>
2023-01-09 04:53:42 -0800

    creat an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		str[i] = c;
+		ptr[i] = c;
 
-	return (str);
+	return (ptr);
 }
